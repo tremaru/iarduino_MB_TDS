279d5869dd32e714f8ebd68047c28717932b1365
TremaUser1 <39560516+TremaUser1@users.noreply.github.com>
2023-05-18 20:27:28 +0300

    1.1.1

--- src/iarduino_MB_TDS.cpp
+++ src/iarduino_MB_TDS.cpp
@@ -82,14 +82,16 @@
 bool	iarduino_MB_TDS::begin(uint8_t id){																					//	id - адрес модуля на шине.
 			objModbus->codeError=ERROR_GATEWAY_NO_DEVICE;																	//	Ошибкой выполнения данной функции может быть только отсутствие устройства.
 		//	Самостоятельный поиск id устройства:																			//
-			if( id==0 )								{ if( objModbus->findID(DEF_MODEL_TDS) ){ id=objModbus->read(); }}		//	Если адрес не указан, ищем адрес первого устройства с совпавшим идентификатором.
+			if( id==0 )								{ if( objModbus->findID(DEF_MODEL_TDS)        ){id=objModbus->read();}}	//	Если адрес не указан, ищем адрес первого устройства с совпавшим идентификатором.
+			if( id==0 )								{ if( objModbus->findID(DEF_MODEL_TDS_zummer) ){id=objModbus->read();}}	//	Если адрес не указан, ищем адрес первого устройства с совпавшим идентификатором.
 		//	Проверяем устройство:																							//
 			if( id==0 )								{ return false; }														//	Адрес устройства не указан и не найден.
 			if( objModbus->getInfo(id)<15 )			{ return false; }														//	Устройство не найдено, или информация об устройстве неполная.
 			if( objModbus->read()!=0x77 )			{ return false; }														//	Идентификатор линейки устройств не соответствует устройствам iArduino.
 				objModbus->read();																							//	Индикатор пуска не проверяем (00=OFF, FF=ON).
 			if( objModbus->read()!=id )				{ return false; }														//	Адрес полученный из информации об устройстве не совпадает с фактическим адресом устройства.
-			if( objModbus->read()!=DEF_MODEL_TDS)	{ return false; }														//	Идентификатор устройства не совпадает с DEF_MODEL_TDS.
+			uint8_t model = objModbus->read();																				//	Получаем идентификатор модели устройства.
+			if( model!=DEF_MODEL_TDS && model!=DEF_MODEL_TDS_zummer ){ return false; }										//	Идентификатор устройства не совпадает с DEF_MODEL_TDS и DEF_MODEL_TDS_zummer.
 		//	Устройство прошло проверку:																						//
 			valID=id;																										//	Сохраняем адрес устройства.
 			valVers=objModbus->read();																						//	Сохраняем версию прошивки устройства.
